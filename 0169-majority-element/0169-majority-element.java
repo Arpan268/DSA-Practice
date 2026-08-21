39c730ca80cac1f0c98581cd025c359073de96d3
Arpan Halder <arpanhalderah3@gmail.com>
2026-08-22 03:01:25 +0530

    Time: 13 ms (32.14%), Space: 52.9 MB (72.39%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,12 +4,7 @@ class Solution {
         int majority = 0;
 
         for(int i = 0; i<nums.length; i++) {
-            if(map.containsKey(nums[i])) {
-                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            }
-            else {
-                map.put(nums[i], 1);
-            }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
 
         for(int n: map.keySet()) {
