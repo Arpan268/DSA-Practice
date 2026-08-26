27b753dbf8656f41a6173ee35bc501685a5fb410
Arpan Halder <arpanhalderah3@gmail.com>
2026-08-27 01:10:10 +0530

    Time: 3 ms (68.61%), Space: 47.8 MB (24.22%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        for(int i = 0; i<nums.length; i++) {
+            while(nums[i] != nums[nums[i]-1]) {
+                int temp = nums[nums[i]-1];
+                nums[nums[i]-1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        for(int i = 0; i<nums.length; i++) {
+            if(i+1 != nums[i]) return new int[] {nums[i], i+1};
+        }
+
+        return new int[0];
+    }
+}
