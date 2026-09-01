9e4672955bb55819d98898bfa94e4092b7b52985
Arpan Halder <arpanhalderah3@gmail.com>
2026-09-02 01:51:10 +0530

    Time: 8 ms (50.57%), Space: 43.6 MB (96.89%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int freq[] = new int[26];
-        int l = s1.length();
+        int s1map[] = new int[26];
+        int s2map[] = new int[26];
 
-        for(int i = 0; i<l; i++) {
-            freq[s1.charAt(i)-'a']++;
+        for(int i = 0; i<s1.length(); i++) {
+            s1map[s1.charAt(i)-'a']++;
         }
 
-        for(int i = 0; i<=s2.length()-l; i++) {
-            int tempfreq[] = freq.clone();
-            int subarray = l;
-            int ans = 0;
-            int j = i;
-            while(subarray > 0) {
-                if(tempfreq[s2.charAt(j)-'a'] > 0) {
-                    ans++;
-                    tempfreq[s2.charAt(j)-'a']--;
-                }
-                j++;
-                subarray--;
+        int left = 0;
+
+        for(int right = 0; right<s2.length(); right++) {
+            s2map[s2.charAt(right)-'a']++;
+
+            if(right-left+1 > s1.length()) {
+                s2map[s2.charAt(left)-'a']--;
+                left++;
             }
-            if(ans == l) return true;
+
+            if(right-left+1 == s1.length()) {
+                if(Arrays.equals(s1map, s2map)) return true;
+            }
         }
 
         return false;
