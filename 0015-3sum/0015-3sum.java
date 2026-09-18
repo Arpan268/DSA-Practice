95e4ca9c5d6834626bcc785c366f028fbc44338e
Arpan Halder <arpanhalderah3@gmail.com>
2026-09-19 02:08:00 +0530

    Time: 29 ms (92.87%), Space: 58.8 MB (90.61%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,14 +4,17 @@ class Solution {
         List<List<Integer>> list = new ArrayList<>();
 
         for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0)
+                break;
+
             int l = i + 1, r = nums.length - 1;
+            int required = -1 * nums[i];
 
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
             while (l < r) {
                 int sum = nums[l] + nums[r];
-                int required = -1 * nums[i];
 
                 if (sum == required) {
                     List<Integer> indices = Arrays.asList(nums[i], nums[l], nums[r]);
