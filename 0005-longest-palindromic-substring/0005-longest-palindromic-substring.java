02d7a7f4aa08c8fdadb99b1a4cd25e80859085fb
Arpan Halder <arpanhalderah3@gmail.com>
2026-09-23 02:11:33 +0530

    Time: 14 ms (92.22%), Space: 43.2 MB (95.05%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,30 +1,26 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        int left = 0, right = 1;
+    private int left = 0;
+    private int right = 1;
 
+    public String longestPalindrome(String s) {
         for (int i = 0; i < s.length(); i++) {
             if (i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) {
-                int l = i, r = i + 1;
-                while (l > -1 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                    l--;
-                    r++;
-                }
-                if (r - l > right - left + 1) {
-                    left = l + 1;
-                    right = r;
-                }
+                checkPalindrome(s, i, i + 1);
             }
-            int l = i - 1, r = i + 1;
-            while (l > -1 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-                l--;
-                r++;
-            }
-            if (r - l > right - left + 1) {
-                left = l + 1;
-                right = r;
-            }
+            checkPalindrome(s, i - 1, i + 1);
         }
 
         return s.substring(left, right);
+    }
+
+    private void checkPalindrome(String s, int l, int r) {
+        while (l > -1 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        if (r - l > right - left + 1) {
+            left = l + 1;
+            right = r;
+        }
     }
 }
